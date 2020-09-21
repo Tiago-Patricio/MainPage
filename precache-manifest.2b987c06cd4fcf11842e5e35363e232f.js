3a2ae39121f1838c65b5d990ee51154c87763bde
Tiago Patricio <69849680+Tiago-Patricio@users.noreply.github.com>
2020-09-21 17:09:22 +0100

    Updates

---
+++ precache-manifest.2b987c06cd4fcf11842e5e35363e232f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbcfccca061c307897ea6ef2ef3afbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51267928dbbc235e4f4a",
+    "url": "/static/css/main.f3c666d9.chunk.css"
+  },
+  {
+    "revision": "8d48e9fdfba59d403e7c",
+    "url": "/static/js/2.1be49de4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1be49de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51267928dbbc235e4f4a",
+    "url": "/static/js/main.d0dffe0e.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
